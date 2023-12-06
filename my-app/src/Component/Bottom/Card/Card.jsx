666c07d22b5d7e29d40262e1a3a42509833bc1f5
Romain GALMIER <romain.galmier@tpg26.cpe.lan>
2023-12-06 11:49:21 +0100

    tree clean

---
+++ my-app/src/Component/Bottom/Card/Card.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import {Label} from './containers/label';
+import {Visual} from './containers/visual';
+export const Robot =(props) =>{
+    return (
+        <div className="panel panel-default" onClick={()=>{props.handleOnRobotSelected(props.robot.id)}}>
+            <div className="panel-heading">
+                <h3 className="panel-title">Robot {props.id} description</h3>
+            </div>
+            <div className="panel-body">
+                <Label 
+                    title={props.robot.title} 
+                    id={props.robot.id} 
+                />
+                <Visual 
+                    type={props.robot.visual_type} 
+                    src={props.robot.visual_src} 
+                />
+            </div>
+        </div>
+        );
+
+}
