7f2b193ccd181311ae5acdf11056c917d0af23e4
Rom9281 <34047259+Rom9281@users.noreply.github.com>
2023-12-09 17:39:22 +0100

    Communicating with server to create account

--- Atelier1/my-app/src/Pages/CreateAccount.jsx
+++ Atelier1/my-app/src/Pages/CreateAccount.jsx
@@ -1,21 +1,53 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 export const CreateAccount = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [money, setMoney] = useState(0);
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-    };
+    const navigate = useNavigate();
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+    const handleSubmit = async (e) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+    
+        const username  = document.querySelector('#formUsername').value;
+        const password  = document.querySelector('#formPassword').value;
+        const email     = document.querySelector('#formEmail').value;
+        const surname   = document.querySelector('#formSurname').value;
+        const lastName  = document.querySelector('#formLastName').value;
+        const money     = document.querySelector('#formMoney').value;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle account creation logic here
+        console.log(username, password, email, surname, lastName, money);
+
+
+        
+        const response = await fetch('http://localhost:80/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id:0,
+                login: username,
+                pwd: password,
+                account:money,
+                lastName: lastName,
+                surName: surname,
+                email: email,
+                cardList:[0],
+
+            }),
+        });
+        if (response.status === 403) {
+            setLoginState("is-invalid");
+        }
+        else if (!response.ok) {
+            console.error('There was an error!', await response.text());
+        } else {
+            const data = await response.json();
+            console.log(data);
+            navigate('/store');
+        }
     };
 
     return (
@@ -27,8 +59,6 @@ export const CreateAccount = () => {
                     <Form.Control
                         type="text"
                         placeholder="Enter username"
-                        value={username}
-                        onChange={handleUsernameChange}
                     />
                 </Form.Group>
 
@@ -37,15 +67,51 @@ export const CreateAccount = () => {
                     <Form.Control
                         type="password"
                         placeholder="Enter password"
-                        value={password}
-                        onChange={handlePasswordChange}
                     />
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                    />
+                </Form.Group>
+
+                
+                <Form.Group controlId="formSurname">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                        type="surname"
+                        placeholder="Enter Surname"
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formLastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                        type="lastName"
+                        placeholder="Enter Last Name"
+                    />
+                </Form.Group>
+                
+                <Form.Group controlId="formMoney">
+                <Form.Label>Money</Form.Label>
+                    <Form.Control
+                        onChange={(e) => setMoney(e.target.value)}
+                        type="range"
+                        min={0}
+                        max={500}
+                        defaultValue={0}
+                        
+                    />
+                    <Form.Label>{money}§</Form.Label>
+                </Form.Group>
+            </Form>
+
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
                     Create Account
                 </Button>
-            </Form>
         </div>
     );
 };
--- Atelier1/my-app/src/Pages/Login.jsx
+++ Atelier1/my-app/src/Pages/Login.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
 
 export const Login = () => {
 
     const navigate = useNavigate();
 
+    const [loginState, setLoginState] = useState("is-neutral");
+
 
     const handleSubmit = async (event) => {
         event.preventDefault(); // Prevent the default form submission behavior
     
-        const email = document.querySelector('#formBasicEmail').value;
+        const username = document.querySelector('#formBasicEmail').value;
         const password = document.querySelector('#formBasicPassword').value;
     
-        const response = await fetch('http://localhost:8083/auth', {
+        const response = await fetch('http://localhost:80/api/auth', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                email: email,
+                username: username,
                 password: password,
             }),
         });
-    
-        if (!response.ok) {
+        if (response.status === 403) {
+            setLoginState("is-invalid");
+        }
+        else if (!response.ok) {
             console.error('There was an error!', await response.text());
         } else {
             const data = await response.json();
@@ -39,13 +44,13 @@ export const Login = () => {
                 <h1>Login</h1>
                 <Form>
                     <Form.Group controlId="formBasicEmail" >
-                        <Form.Label >Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Label >Login</Form.Label>
+                        <Form.Control className={loginState} type="email" placeholder="Enter login" />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control className={loginState} type="password" placeholder="Password" />
                     </Form.Group>
 
                     <Button onClick={handleSubmit} variant="primary" type="submit">
